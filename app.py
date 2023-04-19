1d26e2a3b6f3b1618376930a6d8fe55663d1b3f3
Qiwei <qiwei.he@saagie.com>
2023-04-19 10:05:24 +0200

    test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 import os
 from saagieapi import SaagieApi
 import json
@@ -24,8 +25,7 @@ to project that you want to launch pipeline:
 
 '''
 
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, url_base_pathname=os.environ["SAAGIE_BASE_PATH"]+"/")
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX], url_base_pathname=os.environ["SAAGIE_BASE_PATH"]+"/")
 
 
 saagie_client = SaagieApi(url_saagie=os.environ["SAAGIE_URL"],
@@ -61,17 +61,27 @@ def update_pipelines(value):
     State(component_id="nb-pipeline", component_property="value"),
     State(component_id="project-dropdown", component_property="value"),
     State(component_id='pipelines', component_property='value'),
-    State(component_id="env-vars", component_property="value"),
+    State(component_id="env-vars-tube-name", component_property="value"),
+    State(component_id="env-vars-diameter", component_property="value"),
+    State(component_id="env-vars-options", component_property="value"),
 )
-def run_piepline(n_clicks, nb_pipelines, project_name, pipeline_name, env_vars):
+def run_piepline(n_clicks, nb_pipelines, project_name, pipeline_name, tube_name, diameter, options):
     changed_ids = [p['prop_id'].split('.')[0] for p in dash.callback_context.triggered]
     button_pressed = 'submit-button-state' in changed_ids
+    json_data = {}
     if button_pressed:
         pipeline_id = saagie_client.pipelines.get_id(pipeline_name=pipeline_name, project_name=project_name)
-        if env_vars:
-            saagie_client.env_vars.bulk_create_for_pipeline(pipeline_id, json.loads(env_vars))
+        if tube_name:
+            json_data = {"tube_name": tube_name, "diameter": diameter}
+            if options:
+                json_data["options"] = options
+            saagie_client.env_vars.bulk_create_for_pipeline(pipeline_id, json_data)
         list_pipeline_status = []
+
         for i in range(int(nb_pipelines)):
+            if (int(nb_pipelines) > 1) & (i > 1):
+                json_data["diameter"] = diameter + i
+                saagie_client.env_vars.bulk_create_for_pipeline(pipeline_id, json_data)
             status = saagie_client.pipelines.run_with_callback(pipeline_id=pipeline_id)
             list_pipeline_status.append(status)
 
@@ -81,42 +91,102 @@ def run_piepline(n_clicks, nb_pipelines, project_name, pipeline_name, env_vars):
 
 
 app.layout = html.Div([
-    html.Div([
-        dcc.Markdown(children=markdown_text)
-    ], style={'padding': 10, 'flex': 1}),
-    html.Div(children=[
-        html.Label('Saagie Project'),
-        dcc.Dropdown(projects_name, id="project-dropdown"),
+    dbc.Tooltip(
+        "Name of the tube, should be a text ",
+        target="env-vars-tube-name",
+    ),
+    dbc.Tooltip(
+        "Diameter of the tube, should be a number ",
+        target="env-vars-diameter",
+    ),
+    dbc.Tooltip(
+        "Number of execution of pipeline. "
+        "If number > 1, then diameter will be incremented by 1 in each execution",
+        target="nb-pipeline",
+    ),
+    dbc.Row(
+        dbc.Col(
+            dcc.Markdown(children=markdown_text),
+            style={"padding": 10}
+        )
+    ),
+    dbc.Row(
+        [
+            dbc.Col(
+                [
+                    html.Label('Saagie Project'),
+                    dcc.Dropdown(projects_name,
+                                 id="project-dropdown"
+                                 )
+                ],
+                width={"offset": 1}
+            ),
+            dbc.Col(html.P("Environment variables of pipelines"))
+        ],
+        justify="center",
+    ),
 
-        html.Br(),
-        html.Label('Saagie Pipeline'),
-        dcc.Dropdown(id="pipelines"),
+    dbc.Row(
+        [
+            dbc.Col(
+                [
+                    html.Label('Saagie Pipeline'),
+                    dcc.Dropdown(id="pipelines"),
+                ],
+                width={"offset": 1}
+            ),
+            dbc.Col(
+                [
+                    html.Label('Tube name: '),
+                    dcc.Input(id="env-vars-tube-name", type='text',
+                              placeholder="Type some text...", required=True)
+                ]
+            )
+        ],
+        justify="center",
+    ),
 
-        html.Br(),
-        html.Label('Environment variables for the pipeline, should be like a json'),
-        dcc.Input(id="env-vars", type='text'),
+    dbc.Row(
+        [
+            dbc.Col(
+                [
+                    html.P('Number of execution of pipelines'),
+                    dcc.Input(id="nb-pipeline", value='1', type='number', min=1, step=1, required=True)
+                ],
+                width={"offset": 1}
+            ),
+            dbc.Col(
+                [
+                    html.Label('Diameter (cm): '),
+                    dcc.Input(id="env-vars-diameter", type='number',
+                              placeholder="Type some number...", min=1, required=True),
+                    dbc.Row(html.Br()),
+                    dbc.Label('Options: '),
+                    dbc.Select(
+                        id="env-vars-options",
+                        options=[
+                            {"label": "Option 1", "value": "1"},
+                            {"label": "Option 2", "value": "2"},
+                            {"label": "Disabled option", "value": "3", "disabled": True},
+                        ],
+                    ),
+                    dbc.Row(html.Br()),
+                    dbc.Button('Refresh Data', href='/', className="me-1"),
+                    dbc.Button(id='submit-button-state', n_clicks=0, children='Submit', className="me-1"),
 
-        html.Br(),
-        html.Label('Number of execution of pipelines'),
-        dcc.Input(id="nb-pipeline", value='1', type='number', min=1, step=1),
+                ]
+            )
+        ],
+        justify="center",
+    ),
+    dbc.Row(
+        dbc.Col(
+            dcc.Markdown(id="output-execution"),
+            style={"padding": 10}
+        )
+    ),
 
-        html.Br(),
-        html.Br(),
-        html.A(html.Button('Refresh Data'), href='/'),
-
-        html.Br(),
-        html.Br(),
-        html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
-
-        html.Br(),
-        html.Br(),
-        dcc.Markdown(id="output-execution")
-
-    ], style={'padding': 10, 'flex': 1})
-
-
-], style={'display': 'flex', 'flex-direction': 'row'})
+], style={"font-family": "Arial", "font-size": "0.9em", "text-align": "center"})
 
 if __name__ == '__main__':
-    print("Running second run_server")
     app.run_server(host='0.0.0.0', debug=True, port=8050)
